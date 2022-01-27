83d7e6729ffa94a51139764077079eeadfffd7f4
AhmadRyahnah <ahmad.riahna@gmail.com>
2022-01-27 07:02:26 +0200

    .

--- user/addToCart.php
+++ user/addToCart.php
@@ -1,17 +1,3 @@
 <?php
 include "../database/connection.php";
-
-
-if (isset($_GET['id'])) {
-
-    $sql = "SELECT * FROM products WHERE id='{$_GET['id']}' ";
-
-
-
-
-$sql = "INSERT INTO  checkout (username, email, password,Date_Created) VALUES ('$username', '$email', '$password','$createDate')";
-}
-
-
-
 ?>
