2996d8bc10a410297cc16678a1498773e1c4b571
task <59747580+toqaswedat@users.noreply.github.com>
2022-01-27 20:02:18 +0200

    create db

--- database/create_db.php
+++ database/create_db.php
@@ -4,5 +4,8 @@ $sql="SELECT * FROM categories";
 $products =  $conn->prepare($sql);
 $products ->execute(); ////////edit condition
 // if(!$products->fetchAll(PDO::FETCH_ASSOC)){
-    "INSERT INTO `categories` IF NOT EXISTS (`id`, `cat_name`, `cat_desc`) VALUES (NULL, 'aya', 'ekfnm')";//   "INSERT INTO products (product_name ,product_price,product_desc,on_sales,stock,cat_id) VALUES(girl,10,sodjkm,0,1) ";
+    $data ="INSERT INTO categories  (cat_name, cat_desc) 
+    VALUES ('aya', 'ekfnm')";//   "INSERT INTO products (product_name ,product_price,product_desc,on_sales,stock,cat_id) VALUES(girl,10,sodjkm,0,1) ";
+    $conn->exec($data); ////////edit condition
 // }
+
