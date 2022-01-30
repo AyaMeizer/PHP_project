054819cc2d20d99458318c23de1d66b262584ced
RaniaAbualnadi <rania.abunnadi@gmail.com>
2022-01-30 17:58:12 +0300

    profile

--- user/userProfile.php
+++ user/userProfile.php
@@ -1,36 +1,36 @@
-<!-- <?php 
-include "../database/connection.php" ;
-$edit = "SELECT * FROM users";
-$data = $conn->query($edit);
- $result = $data->fetch(PDO::FETCH_ASSOC);
-?>
+<!-- <?php
+        include "../database/connection.php";
+        $edit = "SELECT * FROM users";
+        $data = $conn->query($edit);
+        $result = $data->fetch(PDO::FETCH_ASSOC);
+        ?>
 
 <!doctype html>
 <html lang="zxx">
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>pillloMart</title>
-    <link rel="icon" href="img/favicon.png">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- animate CSS -->
-    <link rel="stylesheet" href="css/animate.css">
-    <!-- owl carousel CSS -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <!-- font awesome CSS -->
-    <link rel="stylesheet" href="css/all.css">
-    <!-- flaticon CSS -->
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/themify-icons.css">
-    <!-- font awesome CSS -->
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <!-- swiper CSS -->
-    <link rel="stylesheet" href="css/slick.css">
-    <!-- style CSS -->
-    <link rel="stylesheet" href="css/style.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>pillloMart</title>
+<link rel="icon" href="img/favicon.png">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- animate CSS -->
+<link rel="stylesheet" href="css/animate.css">
+<!-- owl carousel CSS -->
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<!-- font awesome CSS -->
+<link rel="stylesheet" href="css/all.css">
+<!-- flaticon CSS -->
+<link rel="stylesheet" href="css/flaticon.css">
+<link rel="stylesheet" href="css/themify-icons.css">
+<!-- font awesome CSS -->
+<link rel="stylesheet" href="css/magnific-popup.css">
+<!-- swiper CSS -->
+<link rel="stylesheet" href="css/slick.css">
+<!-- style CSS -->
+<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -41,9 +41,7 @@ $data = $conn->query($edit);
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg navbar-light">
                         <a class="navbar-brand" href="index.php"> <img src="img/logo.png" alt="logo"> </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="menu_icon"><i class="fas fa-bars"></i></span>
                         </button>
 
@@ -56,25 +54,23 @@ $data = $conn->query($edit);
                                     <a class="nav-link" href="about.html">about</a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_1"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         product
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
                                         <a class="dropdown-item" href="product_list.php"> product list</a>
                                         <a class="dropdown-item" href="single-product.php?id=1">product details</a>
-                                        
+
                                     </div>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_3"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         pages
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                        <a class="dropdown-item" href="login.php"> 
+                                        <a class="dropdown-item" href="login.php">
                                             login
-                                            
+
                                         </a>
                                         <a class="dropdown-item" href="checkout.php">product checkout</a>
                                         <a class="dropdown-item" href="cart.php">shopping cart</a>
@@ -82,10 +78,9 @@ $data = $conn->query($edit);
                                         <a class="dropdown-item" href="elements.php">elements</a>
                                     </div>
                                 </li>
-                                
+
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_2"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         blog
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
@@ -93,7 +88,7 @@ $data = $conn->query($edit);
                                         <a class="dropdown-item" href="single-blog.php">Single blog</a>
                                     </div>
                                 </li>
-                                
+
                                 <li class="nav-item">
                                     <a class="nav-link" href="contact.php">Contact</a>
                                 </li>
@@ -120,7 +115,7 @@ $data = $conn->query($edit);
                 </div>
             </div>
         </div>
-    
+
     </header>
     <!-- Header part end-->
 
@@ -138,76 +133,165 @@ $data = $conn->query($edit);
     </section>
     <!-- breadcrumb part end-->
 
-  
 
 
-      <div class="row">
+
+    <div class="row">
         <div class="col-12">
 
         </div>
 
-    
-                                      
-                                            
+
+
+
 
         <div class="col-lg-8" style='margin-left: 280px;
              display: flex;
              align-items: center;
              flex-direction: column'>
-          <form class="form-contact contact_form" action="<?php echo $_SERVER["PHP_SELF"]?>" method="POST" id="contactForm">
-            <div class="row">
-              <div class="col-12">
-                <div class="form-group">
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <input class="form-control" name="username" id="name" type="text" value="<?php  echo $result['username'] ?>" placeholder='Name'>
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <input class="form-control" name="email" id="" type="text" placeholder='Email' value="<?php echo $result['email'] ?>" readonly></div>
-              </div>
-              <div class="col-12">
-                <div class="form-group">
-                  <input class="form-control" name="password" id="" type="text" value="<?php echo $result['password'] ?>" placeholder='Password'>
-                </div>
-            
-                <div class="form-group">
-                  <input class="form-control" name="phone" id="" type="number" value="<?php echo $result['phone'] ?>" placeholder='Phone'>
-                </div>
-              </div>
-            </div>
-            <div class="form-group mt-3">
-              <input type="submit" class="btn_3 button-contactForm" name="update">
-            </div>
-          </form>
-          
-          <?php 
-                      
+            <form class="form-contact contact_form" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST" id="contactForm">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <input class="form-control" name="username" id="name" type="text" value="<?php echo $result['username'] ?>" placeholder='Name'>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <input class="form-control" name="email" id="" type="text" placeholder='Email' value="<?php echo $result['email'] ?>" readonly>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <input class="form-control" name="password" id="" type="text" value="<?php echo $result['password'] ?>" placeholder='Password'>
+                        </div>
 
-          if($_SERVER['REQUEST_METHOD']=="POST"){
-              $userName = $_POST['username'];
-              $password = $_POST['password'];
-              $phone = $_POST['phone'];
-              $sql = "UPDATE users SET username='$userName', password='$password', phone='$phone'";
-              $conn->exec($sql);   
-              echo "<script>window.location.href='http://localhost/PHP-project/PHP_project/user/userProfile.php'</script>";
-          }
-      
-          ?>
-          
+                        <div class="form-group">
+                            <input class="form-control" name="phone" id="" type="number" value="<?php echo $result['phone'] ?>" placeholder='Phone'>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group mt-3">
+                    <input type="submit" class="btn_3 button-contactForm" name="update">
+                </div>
+            </form>
+
+            <?php
+
+
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                $userName = $_POST['username'];
+                $password = $_POST['password'];
+                $phone = $_POST['phone'];
+                $sql = "UPDATE users SET username='$userName', password='$password', phone='$phone'";
+                $conn->exec($sql);
+                echo "<script>window.location.href='http://localhost/PHP-project/PHP_project/user/userProfile.php'</script>";
+            }
+
+            ?>
+
         </div>
         <div class="col-lg-4">
-          
-      </div>
-    </div>
-  </section>
-  <!-- ================ contact section end ================= -->
 
-  <!--::footer_part start::-->
-  <footer class="footer_part">
+        </div>
+    </div>
+    </section>
+
+
+    <div class="main-content" style='margin-left: 150px ;
+       
+         
+             '>
+        <div class="section__content section__content--p30">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="order_details_iner">
+
+                            <h3 style='margin:5%  40%;
+           
+             flex-direction: column'>Order Details</h3>
+                            <table class="table table-borderless">
+                                <thead>
+                                    <tr>
+
+                                        <th scope="col" colspan="2">Product</th>
+                                        <th scope="col">Quantity</th>
+                                        <th scope="col">Total</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <?php
+                                        $sql = $conn->prepare("SELECT * FROM checkout_products INNER JOIN products ON checkout_products.product_id = products.id");
+                                        $sql->execute();
+                                        $sql3 = $conn->prepare("SELECT * FROM checkout INNER JOIN users ON checkout.user_id = users.id");
+                                        $sql3->execute();
+                                        $data3 = $sql3->fetch(PDO::FETCH_ASSOC);
+
+                                        $sql4 = $conn->prepare("SELECT * FROM checkout
+                                                --  INNER JOIN checkout_products ON checkout.id = checkout_products.checkout_id
+                                                 ");
+                                        $sql4->execute();
+                                        $data4 = $sql4->fetchAll(PDO::FETCH_ASSOC);
+                                        ?>
+                                        <?php
+                                        $tot2 = 0;
+                                        foreach ($sql as $item) {
+                                            $tot = ($item['quantity'] * $item['product_price'] - $item['sales_percentage'] * $item['product_price'] / 100)
+                                        ?>
+
+                                            <td colspan="2"><span><?php echo $item['product_name'] ?></span></td>
+                                            <td>x<?php echo $item['quantity']; ?></td>
+                                            <td> <span><?php echo '$ ' . $tot; ?></span></td>
+                                            <td> <span><?php
+                                                        echo $data3['username'];
+                                                        $tot2 += $tot;
+                                                        ?></span></td>
+                                    </tr>
+
+                                </tbody>
+                                <tfoot>
+                                </tfoot>
+                            <?php
+                                        };
+                            ?>
+                            </table>
+                            <table class="table table-borderless">
+                                <tr>
+                                    <hr />
+                                    <th scope="col" colspan="2"></th>
+                                    <th scope="col" colspan="2">Total</th>
+                                    <th scope="col" colspan="2"></th>
+                                    <th scope="col" colspan="2"></th>
+                                    <th><?php
+
+                                        echo $tot2; ?></th>
+
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+    </div> <!-- ================ contact section end ================= -->
+
+    <!--::footer_part start::-->
+    <footer class="footer_part">
         <div class="footer_iner section_bg">
             <div class="container">
                 <div class="row justify-content-between align-items-center">
@@ -237,15 +321,19 @@ $data = $conn->query($edit);
                 </div>
             </div>
         </div>
-        
+
         <div class="copyright_part">
             <div class="container">
                 <div class="row ">
                     <div class="col-lg-12">
                         <div class="copyright_text">
-                            <P><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></P>
+                            <P>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </P>
                             <div class="copyright_link">
                                 <a href="#">Turms & Conditions</a>
                                 <a href="#">FAQ</a>
@@ -258,8 +346,8 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
     </footer>
     <!--::footer_part end::-->
 
-   <!-- jquery plugins here-->
-   <script src="js/jquery-1.12.1.min.js"></script>
+    <!-- jquery plugins here-->
+    <script src="js/jquery-1.12.1.min.js"></script>
     <!-- popper js -->
     <script src="js/popper.min.js"></script>
     <!-- bootstrap js -->
@@ -280,7 +368,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
     <script src="js/contact.js"></script>
     <script src="js/jquery.ajaxchimp.min.js"></script>
     <!-- <script src="js/jquery.form.js"></script> -->
-    <script src="js/jquery.validate.min.js"></script> 
+    <script src="js/jquery.validate.min.js"></script>
     <script src="js/mail-script.js"></script>
     <!-- custom js -->
     <script src="js/custom.js"></script>
